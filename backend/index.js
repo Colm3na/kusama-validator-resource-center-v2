ae03eb55c0f88c9af2b7180be566159c79681eab
mariopino <info@quequiereshacer.es>
2021-01-21 11:01:26 +0100

    yarn workspaces

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// Required imports
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+async function main () {
+  // Initialise the provider to connect to the local node
+  const provider = new WsProvider('wss://kusama-rpc.polkadot.io');
+
+  // Create the API and wait until ready
+  const api = await ApiPromise.create({ provider });
+
+  // Retrieve the chain & node information information via rpc calls
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
+  ]);
+
+  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+}
+
+main().catch(console.error).finally(() => process.exit());
