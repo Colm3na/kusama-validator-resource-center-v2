cb0113aefb369f633f65c863c9fcd8ecb3b81d05
mariopino <info@quequiereshacer.es>
2021-01-21 19:23:11 +0100

    fix

--- backend/docker/backend/scripts/clean-dockers.sh
+++ backend/docker/backend/scripts/clean-dockers.sh
@@ -7,7 +7,7 @@ docker rm backend_crawler_1
 docker rm backend_postgres_1
 docker rm backend_graphql-engine_1
 
-docker rmi backend:latest
+docker rmi crawler:latest
 
 docker volume rm backend_db-data
 
