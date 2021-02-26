f6670a390a520474a74cca4f3bde0ef93f4e17c4
mariopino <mariopino@protonmail.com>
2021-02-26 13:39:44 +0100

    debug

--- backend/lib/crawlers/ranking.js
+++ backend/lib/crawlers/ranking.js
@@ -483,7 +483,7 @@ module.exports = {
           : new BigNumber(0);
         
         // performance
-        logger.info(loggerOptions, `Validator ${stashAddress} performance ${performance.toFixed(6)}, validatorAverageEraPoints: ${validatorAverageEraPoints}, commission: ${commission} totalStake: ${totalStake.div(new BigNumber(10).pow(config.tokenDecimals)).toNumber()}`);
+        logger.info(loggerOptions, `Validator ${stashAddress} performance ${performance.toFixed(6)}, eraPointsPercent: ${eraPointsPercent}, commission: ${commission}, current totalStake: ${totalStake.div(new BigNumber(10).pow(config.tokenDecimals)).toNumber()}`);
         if (performance > maxPerformance) {
           maxPerformance = performance
         }
