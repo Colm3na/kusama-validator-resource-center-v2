240d44058758efab0e776cfccc392c6ff72e7c14
mariopino <mariopino@protonmail.com>
2021-03-09 21:54:20 +0100

    import page

--- frontend/pages/import.vue
+++ frontend/pages/import.vue
@@ -2,18 +2,65 @@
   <div class="page container-fluid pt-3">
     <div>
       <h1 class="mb-4">Import validator set</h1>
+      <div v-if="loading">
+        <p class="py-4 text-center d-block">
+          Loading addresses from extension...
+        </p>
+      </div>
+      <div v-else>
+        <div v-for="account in extensionAccounts" :key="account.address">
+          <div class="row mx-0 mb-4" style="border: 1px solid gray">
+            <div class="col-md-4 p-3">
+              <Identicon :address="account.address" :size="24" />
+              {{ shortAddress(account.address) }}
+            </div>
+            <div class="col-md-4 p-3">
+              {{ account.role }}
+            </div>
+            <div class="col-md-4 p-3">
+              {{ account.available }}
+            </div>
+          </div>
+          <div v-if="account.staking" class="mb-4 pl-4">
+            <h4>Validator set:</h4>
+            <div
+              v-for="validator in account.staking.targets"
+              :key="`${account.address}-${validator}`"
+              class="d-block my-1"
+            >
+              {{ validator }}
+            </div>
+            <b-button variant="outline-primary2" class="my-4"
+              >IMPORT SET</b-button
+            >
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { config } from '@/config.js'
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp'
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { encodeAddress } from '@polkadot/keyring'
+import Identicon from '@/components/Identicon.vue'
 import commonMixin from '@/mixins/commonMixin.js'
+import { config } from '@/config.js'
 export default {
+  components: { Identicon },
   mixins: [commonMixin],
   data() {
     return {
       config,
+      detectedExtension: false,
+      extensionAccounts: [],
+      extensionAddresses: [],
+      api: null,
+      enableWeb3: false,
+      error: null,
+      noAccountsFound: true,
+      loading: true,
     }
   },
   head() {
@@ -27,6 +74,87 @@ export default {
         },
       ],
     }
+  },
+  async created() {
+    if (this.$store.state.ranking.list.length === 0) {
+      await this.$store.dispatch('ranking/update')
+    }
+    this.enableWeb3 = await web3Enable(
+      `${config.title} for ${this.capitalize(config.name)}`
+    )
+      .then(() => {
+        web3Accounts()
+          .then((accounts) => {
+            const wsProvider = new WsProvider(config.nodeWs)
+            ApiPromise.create({ provider: wsProvider }).then(async (api) => {
+              this.api = api
+              if (accounts.length > 0) {
+                this.detectedExtension = true
+                for (const account of accounts) {
+                  const address = encodeAddress(
+                    account.address,
+                    config.addressPrefix
+                  )
+                  const balances = await this.getAccountBalances(address)
+                  this.extensionAccounts.push({
+                    address,
+                    role: await this.getAddressRole(address),
+                    staking: await this.getStakingInfo(address),
+                    available: this.formatAmount(balances.availableBalance),
+                    selected: false,
+                  })
+                }
+                if (
+                  this.extensionAccounts.length > 0 &&
+                  this.extensionAddresses.length > 0
+                ) {
+                  this.noAccountsFound = false
+                } else {
+                  this.noAccountsFound = true
+                }
+                this.loading = false
+              }
+            })
+          })
+          .catch((error) => {
+            // eslint-disable-next-line
+            console.log('Error: ', error)
+          })
+      })
+      .catch((error) => {
+        // eslint-disable-next-line
+        console.log('Error: ', error)
+      })
+  },
+  methods: {
+    async getAccountBalances(address) {
+      const balances = await this.api.derive.balances.all(address)
+      return balances
+    },
+    async getStakingInfo(address) {
+      const staking = await this.api.query.staking.nominators(address)
+      return JSON.parse(JSON.stringify(staking))
+    },
+    async getAddressRole(address) {
+      const bonded = await this.api.query.staking.bonded(address)
+      if (bonded.toString() && bonded.toString() === address) {
+        return `stash/controller`
+      } else if (bonded.toString() && bonded.toString() !== address) {
+        return `stash`
+      } else {
+        const stakingLedger = await this.api.query.staking.ledger(address)
+        if (stakingLedger.toString()) {
+          return `controller`
+        } else {
+          return `none`
+        }
+      }
+    },
+    async selectAddress(address) {
+      await this.$store.dispatch('ranking/updateSelectedAddress', address)
+      this.$emit('close')
+      return true
+    },
   },
 }
 </script>
