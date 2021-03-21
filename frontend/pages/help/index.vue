44f26f24c51a562319904ef8ebbdd4250294948a
mariopino <mariopino@protonmail.com>
2021-03-21 11:51:05 +0100

    link to metrics in help

--- frontend/pages/help/index.vue
+++ frontend/pages/help/index.vue
@@ -3,6 +3,11 @@
     <div>
       <h1 class="mb-4">Help</h1>
       <p>General information about user interface.</p>
+      <h2>Metrics</h2>
+      <p>
+        Check the definition of every metric included in the Validator Resource
+        Center <nuxt-link to="/help/metrics">here</nuxt-link>
+      </p>
     </div>
   </div>
 </template>
