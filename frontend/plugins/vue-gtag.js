3864d3f8f7c0a04efd1dc4366072f8e97dae3213
mariopino <mariopino@protonmail.com>
2021-04-01 14:28:22 +0200

    cleanup

--- frontend/plugins/vue-gtag.js
+++ frontend/plugins/vue-gtag.js
@@ -3,10 +3,18 @@ import VueGtag from 'vue-gtag'
 import { config } from '@/config.js'
 
 export default ({ app }) => {
+  const kusamaValidatorsNetwork = JSON.parse(
+    decodeURIComponent(localStorage.getItem('kusamaValidatorsNetwork'))
+  )
   Vue.use(
     VueGtag,
     {
-      config: { id: config.googleAnalytics },
+      config: {
+        id: config.googleAnalytics,
+        disabled: kusamaValidatorsNetwork
+          ? !kusamaValidatorsNetwork.googleAnalytics
+          : true,
+      },
     },
     app.router
   )
